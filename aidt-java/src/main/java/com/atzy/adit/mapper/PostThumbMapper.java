374369d052018b41d6fe231b2c884588370043e8
lgl <liguilin@aliyun.com>
2024-12-13 14:16:32 +0800

    aidt-初始搭建

---
+++ aidt-java/src/main/java/com/atzy/adit/mapper/PostThumbMapper.java
@@ -0,0 +1,16 @@
+package com.atzy.adit.mapper;
+
+import com.atzy.adit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ aidt-java/src/main/java/com/atzy/adit/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.atzy.adit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.atzy.adit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
